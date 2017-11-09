4eef2b91a2704c862e29ded99015b997d8c9fd9a
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-09 18:56:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'john'
+
+function upperCaseCustomerName(){
+  customerName = 'BOB';
+
+}
+
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+
+}
+
+function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'saly'
+}
+
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'musa';
+  let favoriteCustomer = 'paul';
+
+}
